a71a31a5a7fa9a8296e37c82905ae79d75e14f76
Jorge Paravicini <j.m.paravicini@gmail.com>
2023-08-07 12:54:50 +0200

    feat: add observable support

--- projects/ngx-choices/src/public-api.ts
+++ projects/ngx-choices/src/public-api.ts
@@ -2,7 +2,7 @@
  * Public API Surface of ngx-choices
  */
 
-export * from './lib/choices.component';
+export * from './lib/choices.directive';
 export * from './lib/ngx-choices.module';
 export * from './lib/ngx-choices.service';
 export * from './lib/types.model';
